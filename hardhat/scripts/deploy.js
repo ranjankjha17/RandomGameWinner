7c1bf3905accb37899634a0e5681aa83d78b0cb1
Ranjan <talk2ranjanktr@gmail.com>
2022-06-30 12:29:50 +0530

    --

---
+++ hardhat/scripts/deploy.js
@@ -0,0 +1,38 @@
+const {ethers}=require("hardhat");
+require("dotenv").config({path:".env"});
+require("@nomiclabs/hardhat-etherscan");
+const {FEE,VRF_COORDINATOR,LINK_TOKEN,KEY_HASH}=require("../constants");
+
+
+async function main(){
+    const randomWinnerGame=await ethers.getContractFactory("RandomWinnerGame");
+
+    const deployedRandomWinnerGameContract=await randomWinnerGame.deploy(VRF_COORDINATOR,LINK_TOKEN,KEY_HASH,FEE);
+
+    await deployedRandomWinnerGameContract.deployed();
+
+    console.log("Verify Contract Address:",deployedRandomWinnerGameContract.address);
+
+    console.log("Sleeping.....");
+
+    await sleep(30000);
+
+    // Verify the contract after deploying
+  await hre.run("verify:verify", {
+    address: deployedRandomWinnerGameContract.address,
+    constructorArguments: [VRF_COORDINATOR, LINK_TOKEN, KEY_HASH, FEE],
+  });
+  
+}
+
+function sleep(ms){
+    return new Promise((resolve)=> setTimeout(resolve,ms));
+}
+
+
+main()
+    .then(()=>process.exit(0))
+    .catch((error)=>{
+        console.error(error);
+        process.exit(1);
+    })
